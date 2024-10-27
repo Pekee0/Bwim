33f6e6299025225d5c1affc02e7a4ce999c4107b
Marcos <marbianchi537@gmail.com>
2024-10-27 20:00:45 -0300

    FrontPage Avanzada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component'
 import { LoginFormComponent } from './Components/login-form/login-form.component';
+import { ScrollRevealComponent } from "./scroll-reveal/scroll-reveal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LoginFormComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginFormComponent, ScrollRevealComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/scroll-reveal/scroll-reveal.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-reveal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './scroll-reveal.component.html',
+  styleUrl: './scroll-reveal.component.css'
+})
+export class ScrollRevealComponent {
+
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPosition = window.scrollY;
+    this.isScrolled = scrollPosition > window.innerHeight * 0.24; // Comienza la transición un poco antes
+  }
+
+}
