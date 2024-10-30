32bd67aca42a3d29967989f6d7993e0f77026c28
Pekee0 <gonza.marsala@gmail.com>
2024-10-30 20:04:17 -0300

    se crea register-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component'
 import { LoginFormComponent } from './Components/login-form/login-form.component';
+import { RegisterFormComponent } from './Components/register-form/register-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LoginFormComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginFormComponent,RegisterFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
