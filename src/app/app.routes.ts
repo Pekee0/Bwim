f1bb52805446ca81046334e18f55e9ed064c6743
pablowalter11 <pablo.walter.quiroga11@gmail.com>
2024-11-04 04:26:45 -0300

    así es como se vería el apartado sobre nostros en bwim

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,23 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { ScrollRevealComponent } from './scroll-reveal/scroll-reveal.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
-
-{
-    path: '',
-    component: ScrollRevealComponent
-},
-{
-    path: 'projects',
-    component: ProjectPageComponent
-},
-{   
-    path: '**',
-    component: ScrollRevealComponent
-}    
+    {
+        path: '',
+        component: ScrollRevealComponent
+    },
+    {
+        path: 'projects',
+        component: ProjectPageComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {   
+        path: '**',
+        component: ScrollRevealComponent
+    }    
 ];
