c5582e723c431fbd0bbe532cec718539e003008c
Marcos <marbianchi537@gmail.com>
2024-11-03 22:08:36 -0300

    Landing Page final, widget clima final, navbar casi final, todo con efectos responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ScrollRevealComponent } from './scroll-reveal/scroll-reveal.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 export const routes: Routes = [
 
-        
+{
+    path: '',
+    component: ScrollRevealComponent
+},
+{
+    path: 'projects',
+    component: ProjectPageComponent
+},
+{   
+    path: '**',
+    component: ScrollRevealComponent
+}    
 ];
