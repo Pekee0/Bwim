f6ac24f7b3e6419961372d7a15f534ebe8e7f565
Pekee0 <gonza.marsala@gmail.com>
2024-11-15 06:01:08 -0300

    test FINALIZADO

--- src/app/Components/mingames-component/test-personalidad/test-personalidad.component.ts
+++ src/app/Components/mingames-component/test-personalidad/test-personalidad.component.ts
@@ -19,13 +19,36 @@ interface Result {
 export class TestPersonalidadComponent {
   
   questions: Question[] = [
-    { questionText: 'What is your favorite color?', options: [
-      { text: 'Red', value: 1 }, { text: 'Blue', value: 2 }, { text: 'Green', value: 3 }, { text: 'Yellow', value: 4 }, { text: 'Purple', value: 5 }
+    { questionText: 'Priorizas y planificas las tareas con eficacia, y sueles terminarlas mucho antes de la fecha límite.', options: [
+      { text: 'No, soy una persona que le gusta improvisar', value: 1 }, { text: 'Tengo mi propia agenda con colores y recordatorios', value: 2 }, { text: 'Siempre encuento a alguien que lo haga por mi', value: 3 }, { text: 'Me organizo tanto a mi como a los que trabajan conmigo', value: 4 }, { text: 'Aprendi y soy una persona meticulosa', value: 5 }
     ]},
-    { questionText: 'What is your favorite animal?', options: [
-      { text: 'Cat', value: 1 }, { text: 'Dog', value: 2 }, { text: 'Bird', value: 3 }, { text: 'Fish', value: 4 }, { text: 'Horse', value: 5 }
+    { questionText: 'Haces nuevos amigos con frecuencia.', options: [
+      { text: 'Como me traten a mi es como los tratare a ellos', value: 1 }, { text: 'Siempre estoy disponible para ayudar a alguien a sentirse incluido', value: 2 }, { text: 'No necesito amigos.', value: 3 }, { text: 'No tengo amigos, solo compañeros', value: 4 }, { text: 'Para amigos como los que tengo, prefiero enemigos', value: 5 }
     ]},
-    // Agrega más preguntas según sea necesario
+    { questionText: 'Después de tu muerte, ¿qué desearías que las personas hagan cuando escuchen tu nombre?', options: [
+      { text: 'Piensen en mis logros', value: 1 }, { text: 'Me recuerden como una buena compañera y amiga', value: 2 }, { text: 'TIEMBLEN.', value: 3 }, { text: 'Nada. Sigan con su vida', value: 4 }, { text: 'Dudo que alguien recuerde mi nombre', value: 5 }
+    ]},
+    { questionText: 'Estas en medio de un examen y ves como alguien se esta copiando ¿Que haces?', options: [
+      { text: 'Yo soy el que se esta copiando', value: 1 }, { text: 'Intendo convencerlo de hacer lo correcto', value: 2 }, { text: 'Los obligo a que me pasen las respuestas a cambio de mi silencio ', value: 3 }, { text: 'Le aviso al docente a cargo', value: 4 }, { text: 'Finjo un incendio para evadir el examen', value: 5 }
+    ]},
+    { questionText: 'Si me invitan a una fiesta, yo...', options: [
+      { text: 'Voy y le pongo toda la onda', value: 1 }, { text: 'Yo soy quien la organiza', value: 2 }, { text: 'Me encierro en el baño y rompo todo', value: 3 }, { text: 'Usaria mi tiempo en una actividad mas productiva', value: 4 }, { text: 'Observo todo encapuchado desde el pasillo', value: 5 }
+    ]},
+    { questionText: 'Vas manejando y el auto de adelante va a un ritmo muy lento', options: [
+      { text: 'Toco la bocina hasta que se apure o se mueva', value: 1 }, { text: 'Me siento mal por acelerar y rebasar el maximo permitido', value: 2 }, { text: 'No se, yo tengo chofer propio', value: 3 }, { text: 'En el proximo semaforo, bajo y respetuosamente le ofrezco darle clases de manejo', value: 4 }, { text: 'Le choco la rueda de atras para que despiste', value: 5 }
+    ]},
+    { questionText: 'Mi color favorito es...', options: [
+      { text: 'Rojo', value: 1 }, { text: 'Rosa', value: 2 }, { text: 'Azul', value: 3 }, { text: 'Verde', value: 4 }, { text: 'Negro', value: 5 }
+    ]},
+    { questionText: 'A una primera cita la/o invito a... ', options: [
+      { text: 'A una ComicCon', value: 1 }, { text: 'A merendar a un cafe re aestethic', value: 2 }, { text: 'Investigo sus gustos, para que piense que somos compatibles', value: 3 }, { text: 'A un restaurante lujoso con un buen vino ', value: 4 }, { text: 'Ya no recuerdo lo que es una cita', value: 5 }
+    ]},
+    { questionText: 'Mi comida favorita es...', options: [
+      { text: 'Milansesa Napolitana', value: 1 }, { text: 'Sushi', value: 2 }, { text: 'Salchicas con pure', value: 3 }, { text: 'Asado', value: 4 }, { text: 'Lo que haya', value: 5 }
+    ]},
+    { questionText: 'Si pudieras elegir un poder, ¿Cual seria?', options: [
+      { text: 'Frenar el tiempo', value: 1 }, { text: 'Poder volar', value: 2 }, { text: 'Control Mental', value: 3 }, { text: 'Estamos grandes para esas cosas', value: 4 }, { text: 'Poder dormir', value: 5 }
+    ]},
   ];
 
   currentQuestionIndex = 0;
