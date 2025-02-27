421c38813a9b66e0c9d99f1e46b1986da48e68b2
Nicolas <nr22673a@gmail.com>
2025-02-27 10:47:40 -0300

    si

---
+++ src/app/Components/comments-components/comments-components.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-components',
+  standalone: true,
+  imports: [],
+  templateUrl: './comments-components.component.html',
+  styleUrl: './comments-components.component.css'
+})
+export class CommentsComponentsComponent {
+
+}
